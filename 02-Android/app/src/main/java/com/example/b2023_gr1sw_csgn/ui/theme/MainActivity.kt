518be1be4b2d01e7ac97d716fdb180422c2c9ee7
SalomeGarzon20 <150186701+SalomeGarzon20@users.noreply.github.com>
2024-01-14 23:18:33 -0500

    Delete 02-Android directory

--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_csgn/ui/theme/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.b2023_gr1sw_csgn.ui.theme
-
-class MainActivity AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-    }
-}
